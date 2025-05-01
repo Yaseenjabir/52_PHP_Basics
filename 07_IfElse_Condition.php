7c899a9c6f5e589b547ed2a873624f79e41d0552
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-01 10:39:56 +0500

    If elseif condition

---
+++ 07_IfElse_Condition.php
@@ -0,0 +1,24 @@
+<?php
+
+// $age = 17;
+
+// if($age < 18){
+//   echo "Your age is less than 18";
+// }
+// else{
+//   echo "You age is more than 20";
+// }
+
+$age2 = 20;
+
+if($age2 < 10){
+  echo "Your age is less than 10";
+}
+elseif($age2 >= 10 && $age2 <= 20){
+  echo "Your age is more than 10 and less than 20";
+}
+else{
+  echo "Your age is more than 20";
+}
+
+?>
