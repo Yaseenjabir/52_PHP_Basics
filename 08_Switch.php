00415771febc1c824f97edc9f345a831db8c2723
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-01 11:32:50 +0500

    Switch statement in PHP

---
+++ 08_Switch.php
@@ -0,0 +1,20 @@
+<?php
+
+$color = "orange";
+switch($color){
+  case "red":
+    echo "This is red color";
+    break;
+
+  case "yellow":
+    echo "This is yellow color";
+    break;  
+
+  case "green":
+    echo "This is green color";
+    break;
+
+  default : 
+  echo "This is unknown color";
+}
+?>
