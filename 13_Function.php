aede13f234a2d706b78359a6a98712fd5c662cc5
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-03 21:54:44 +0500

    Function

---
+++ 13_Function.php
@@ -0,0 +1,18 @@
+<?php
+
+function displayData(){
+  echo "Name : Yaseen Jabir";
+  echo "<br />";
+  echo "Age : 20";
+  echo "<br />";
+  echo "None: none";
+  echo "<br />";
+};
+
+displayData();
+displayData();
+displayData();
+displayData();
+displayData();
+
+?>
