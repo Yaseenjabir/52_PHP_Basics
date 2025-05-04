f6636e3d41e91636424a26e792c28a77e4d773fa
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-04 10:36:59 +0500

    PHP Built-in functions

---
+++ 17_buitIn_functions.php
@@ -0,0 +1,38 @@
+<?php
+
+// Check length of string
+$str = "yaseen jabir";
+// echo strlen($str), "<br />";
+
+// Check date
+// echo date("m"),"<br />";
+
+// setup info
+// echo phpinfo(), "<br />";
+
+// Info about data type
+// echo var_dump($str);
+
+// check is string or not
+// echo is_string($str); // Returns 1 if true, Returns nothing if false
+
+// check is integar or not
+// $varr = 1234;
+// echo is_int($varr);
+
+// generate random number
+// echo rand()
+
+// stop code execution
+/*
+echo "hello world";
+echo "hello world";
+echo "hello world";
+die(); //  will execute only to this line not the below code
+echo "hello world";
+*/
+
+// sub string
+echo substr($str,5);
+
+?>
