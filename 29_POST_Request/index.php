24e56ae4be3e9cfcb3237db3e1cf34ac37faffa2
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-13 21:22:39 +0500

    POST request in php

---
+++ 29_POST_Request/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+echo "Username is : ".$username."<br /> Email is : ".$email."<br /> Password is : ".$password;
+
+?>
