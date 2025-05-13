3db2a80b9d723246f96ecc2d15a0f1d2a2822497
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-13 16:46:09 +0500

    Require and include

---
+++ 27_Include_And_require.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "This is a file which is requiring another file";
+
+echo "<br />";
+
+// include("./26_Global_And_Local_Variables.php");  If I write this statement multipe time it will include the code multiple time and if the path is wrng it will throw error
+
+// include_once("./26_Global_And_Local_Variables.php");  If I write this statement multipe time it will include the code only once and if the path is wrng it will throw error
+
+// require("./26_Global_And_Local_Variables.php");  If I write this statement multipe time it will include the code multiple time and if the path is wrng it will throw error
+
+// require("./26_Global_And_Local_Variables.php");  If I write this statement multipe time it will include the code multiple time and if the path is wrng it will throw error
+?>
